d604d319145c97ba3c8350a6d2600d3d0ff50182
Sopan Mittal <sopan.mittal@zopsmart.com>
2020-09-01 13:26:49 +0530

    add CORS

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,23 @@ import * as Koa from 'koa';
 import * as logger from 'koa-logger';
 import * as json  from 'koa-json';
 import * as bodyparser from 'koa-bodyparser';
+const cors = require('@koa/cors');
 
 import { config } from './config';
 import DB from './connection';
 
 import cart from './routes/cart';
 import products from './routes/products';
+import router from './routes/cart';
 
 const app = new Koa();
 const PORT = config.port;
 
 app.use(bodyparser());
 app.use(logger());
+app.use(cors({}));
 
-app.use(cart.routes());
+app.use(cart.routes()).use(router.allowedMethods());
 app.use(products.routes());
 
 app.listen(PORT, () => {
